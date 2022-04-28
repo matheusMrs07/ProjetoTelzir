a4778e657591c840cc60e0d2250c0c8ba2920284
matheusMrs07 <morescomatheus77@gmail.com>
2022-04-28 14:43:30 -0300

    correção docker

---
+++ db/projetoTelzir.sql
@@ -0,0 +1,20 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: mysql
+-- Generation Time: Jun 27, 2019 at 01:14 AM
+-- Server version: 5.7.26
+-- PHP Version: 7.2.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
